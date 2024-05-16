00cebd47ea86b513425a7bd99c17fe72657f6d3a
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2024-05-16 12:25:56 +0700

    Add rest get using http provider

--- lib/models/http_provider.dart
+++ lib/models/http_provider.dart
@@ -1,3 +1,19 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-class HttpProvider with ChangeNotifier {}
+class HttpProvider with ChangeNotifier {
+  Map<String, dynamic> _data = {};
+  Map<String, dynamic> get data => _data;
+  int get jumlahData => _data.length;
+
+  void connectAPI(String id) async {
+    Uri url = Uri.parse('https://reqres.in/api/users/$id');
+
+    var hasilResponse = await http.get(url);
+
+    _data = (json.decode(hasilResponse.body))['data'];
+
+    notifyListeners();
+  }
+}
--- lib/pages/home_provider.dart
+++ lib/pages/home_provider.dart
@@ -1,11 +1,16 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:provider_get_flutter/models/http_provider.dart';
 
 class HomeProvider extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final dataProvider = Provider.of<HttpProvider>(context, listen: false);
     return Scaffold(
       appBar: AppBar(
-        title: Text("GET - STATEFUL"),
+        title: Text("GET - PROVIDER"),
       ),
       body: Container(
         width: double.infinity,
@@ -18,42 +23,60 @@ class HomeProvider extends StatelessWidget {
               child: Container(
                 height: 100,
                 width: 100,
-                child: Image.network(
-                  "https://www.uclg-planning.org/sites/default/files/styles/featured_home_left/public/no-user-image-square.jpg?itok=PANMBJF-",
-                  fit: BoxFit.cover,
+                child: Consumer<HttpProvider>(
+                  builder: (context, value, child) => Image.network(
+                    (value.data['avatar'] == null)
+                        ? "https://i.pinimg.com/originals/95/a9/4d/95a94ddbe91f1ad5e59498e44c4c13e6.jpg"
+                        : value.data['avatar'],
+                    fit: BoxFit.cover,
+                  ),
                 ),
               ),
             ),
             SizedBox(height: 20),
             FittedBox(
-              child: Text(
-                "ID : Belum ada data",
-                style: TextStyle(fontSize: 20),
+              child: Consumer<HttpProvider>(
+                builder: (context, value, child) => Text(
+                  (value.data['id'] == null)
+                      ? "ID : Belum ada data"
+                      : "ID : ${value.data['id']}",
+                  style: TextStyle(fontSize: 20),
+                ),
               ),
             ),
             SizedBox(height: 20),
             FittedBox(child: Text("Name : ", style: TextStyle(fontSize: 20))),
             FittedBox(
-              child: Text(
-                "Belum ada data",
-                style: TextStyle(
-                  fontSize: 20,
+              child: Consumer<HttpProvider>(
+                builder: (context, value, child) => Text(
+                  (value.data['fullname'] == null)
+                      ? "Belum ada data"
+                      : "${value.data['fullname']}",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
             ),
             SizedBox(height: 20),
             FittedBox(child: Text("Email : ", style: TextStyle(fontSize: 20))),
             FittedBox(
-              child: Text(
-                "Belum ada data",
-                style: TextStyle(
-                  fontSize: 20,
+              child: Consumer<HttpProvider>(
+                builder: (context, value, child) => Text(
+                  (value.data['email'] == null)
+                      ? "Belum ada data"
+                      : "${value.data['email']}",
+                  style: TextStyle(
+                    fontSize: 20,
+                  ),
                 ),
               ),
             ),
             SizedBox(height: 100),
             OutlinedButton(
-              onPressed: () {},
+              onPressed: () {
+                dataProvider.connectAPI((1 + Random().nextInt(10)).toString());
+              },
               child: Text(
                 "GET DATA",
                 style: TextStyle(
